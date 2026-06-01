ce2580759b3906b2060482c9c8c77a4259e0b321
quan-du <s4099918@rmit.edu.vn>
2026-06-01 22:01:44 +0700

    Set up basic project folder

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+// Importing dependencies: 
+const express = require('express');
+const cors = require('cors');
+const 
+const app = express();
+
+
+// Listening
+app.listen(3000, ()=>{
+    console.log("App is running at 3000!")
+})
